f71d380bb3131f72d92c51a9fc9f2875a5a69227
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2024-03-25 11:58:48 +0100

    done
    don't forget module exports at the bottom of the file

--- server/routes/people.js
+++ server/routes/people.js
@@ -1,24 +1,24 @@
 const express = require('express');
 
 const router = express.Router();
-
+// /people
 router.get("/", (req, res) => {
   res.json("Number 1");
 });
-
+// /people/:personId/likes
 router.post("/:personId/likes", (req, res) => {
   res.json("Number 2");
-});
-
+})
+// /people/:personId
 router.delete("/:personId", (req, res) => {
   res.json("Number 3");
 });
-
+// /people/best-dressed/comments
 router.get("/best-dressed/comments", (req, res) => {
   res.json("Number 4");
 });
-
-router.get("/people/:name/lookup", (req, res) => {
+// /people/:name/lookup
+router.get("/:name/lookup", (req, res) => {
   res.json("Number 5");
 });
 
